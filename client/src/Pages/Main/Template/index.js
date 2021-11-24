1dd4da8e952e187e01264c5539334fd56108266d
hanteagyu <snowone4426@gmail.com>
2021-11-24 11:24:55 +0900

    [client] ADD: 아토믹 패턴을 활용한 클라이언트 초기 세팅

---
+++ client/src/Pages/Main/Template/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function MainTemplate () {
+  return (
+    <Container>
+      Hello, World!!
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  position: relative;
+`;
