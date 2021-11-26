581f07abaa7f83be235cdd14d8a088ca0ef0c861
Seok-CH <tjr6677@gmail.com>
2021-11-26 14:26:46 +0900

    [client] ADD: PostAlign 스토리 생성

--- client/src/Components/Atoms/Button/PostAlign.js
+++ client/src/Components/Atoms/Button/PostAlign.js
@@ -1,32 +1,38 @@
 import React from "react";
 import { FaRegClock, FaFireAlt } from "react-icons/fa";
+import styled from "styled-components";
 
-export default function PostAlign(
-  backgroundColor = "white",
-  size = "md",
-  type = "recent",
-  active,
-  handleClick
-) {
-  const icons = type === "recent" ? <FaRegClock /> : <FaFireAlt />;
-  const label = type === "recent" ? "최신" : "인기";
+export default function PostAlign({
+  size = "lg",
+  alignType = "recent",
+  handleClick,
+}) {
+  const icons = alignType === "recent" ? <FaRegClock /> : <FaFireAlt />;
+  const label = alignType === "recent" ? "최신" : "인기";
 
   let scale = 1;
-  if (scale === "sm") scale = 0.75;
-  if (scale === "lg") scale = 1.5;
+  if (size === "sm") scale = 0.75;
+  if (size === "lg") scale = 1.5;
 
-  const style = {
-    display: "flex",
-
-    width: `${scale * 2}rem`,
-    height: `${scale * 1}rem`,
-
-    backgroundColor,
-  };
   return (
-    <button onClick={handleClick} style={style}>
+    <PostAlignButton onClick={handleClick} scale={scale}>
       {icons}
       {label}
-    </button>
+    </PostAlignButton>
   );
 }
+
+const PostAlignButton = styled.div`
+  display: flex;
+
+  width: ${(props) => props.scale * 5}rem;
+  height: ${(props) => props.scale * 2}rem;
+
+  font-size: ${(props) => props.scale}rem;
+  font-weight: bold;
+  line-height: 0.9;
+
+  svg {
+    margin-right: 0.3rem;
+  }
+`;
