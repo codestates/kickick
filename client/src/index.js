f8d80135e360d62cfc1228b9f86b39159c509e77
hanteagyu <snowone4426@gmail.com>
2021-11-29 18:10:01 +0900

    [client] MOD: 폴더 파싱 정정

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import GlobalStyles from "./commons/Styles/GlobalStyles";
+import GlobalStyles from "./commons/styles/GlobalStyles";
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
