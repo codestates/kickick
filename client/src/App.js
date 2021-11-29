26b060784a60576e1d8f204ff40d76249c5e5117
Seok-CH <tjr6677@gmail.com>
2021-11-29 13:47:05 +0900

    [client] MOD: 폴더 이름 camelCase로 변경

--- client/src/App.js
+++ client/src/App.js
@@ -9,8 +9,8 @@ import { light, dark } from "./commons/Styles/Theme";
 
 export default function App() {
   // NOTICE useAxios, api 모듈화 Test 용 밑에꺼 주석 해제후 테스트해보세요
-  // const { loading, data, msg, error } = useAxios(getPostsInfo(1));
-  // console.log(loading, data, msg, error);
+  const { loading, data, msg, error } = useAxios(getPostsInfo(1));
+  console.log(loading, data, msg, error);
 
   // NOTICE 아래는 아직 없는 컴포넌트 이지만 로딩중일때 로딩화면이 뜨는 컴포넌트를 사용하는 예시
   // if (loading) return <LoadingSpinner />
--- client/src/commons/styles/theme.js
+++ client/src/commons/styles/theme.js
@@ -45,7 +45,18 @@ const common = {
 
 export const light = {
   ...common,
-  color: {},
+  color: {
+    back: "#ffffff",
+    subBack: "#8A8A9B",
+    main: "#0c0c42",
+    font: "#222222",
+    border: "#c4c4c4",
+    navBtn: "#350480",
+    basicBtn: "#0c0c42",
+    modalBtn: "#82B0F5",
+    comment: "#3467ce",
+    tag: "82B0F5",
+  },
 };
 
 export const dark = {
