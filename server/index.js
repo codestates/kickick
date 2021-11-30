4d74c532e4a43c2a5354db14fe544750f722ce19
he2mo <sivakim91@gmail.com>
2021-11-30 15:02:58 +0900

    [server] ADD: 즐겨찾기 api 구현

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const kicks_router = require("./routers/kicks_router");
 const tags_router = require("./routers/tags_router");
 const comments_router = require("./routers/comments_router");
 const likes_router = require("./routers/likes_router");
+const favorites_router = require("./routers/favorites_router");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,6 +35,7 @@ app.use("/kicks", kicks_router);
 app.use("/tags", tags_router);
 app.use("/comments", comments_router);
 app.use("/likes", likes_router);
+app.use("/favorites", favorites_router);
 
 app.get("/", (req, res) => {
   res.status(201).send("Hello World");
