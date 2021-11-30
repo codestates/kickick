9845eb88334dcf3a39c32536e8dfaf015c982d76
gozld5153 <gozld5153@naver.com>
2021-11-30 14:28:05 +0900

    [client] MODIFY: app폴더명 store로 변경

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import GlobalStyles from "./commons/styles/GlobalStyles";
-import { store } from "./app/store";
+import { store } from "./store/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 
