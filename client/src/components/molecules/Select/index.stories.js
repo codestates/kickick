803e8d9a260214bdfc1ca3d8748fc6ab6128df80
gozld5153 <gozld5153@naver.com>
2021-12-01 21:12:14 +0900

    [client] MOD:병합출동해결

--- client/src/components/molecules/Select/index.stories.js
+++ client/src/components/molecules/Select/index.stories.js
@@ -3,11 +3,7 @@ import React from "react";
 import Select from "./";
 
 export default {
-<<<<<<< HEAD:client/src/components/molecules/Select/Options.stories.js
-  title: "Molecules/Select/Options",
-=======
   title: "Molecules/Select",
->>>>>>> a8abcbc77e0aacc6ff351b54a366f4bb2f542fa6:client/src/components/molecules/Select/index.stories.js
   component: Select,
 };
 
