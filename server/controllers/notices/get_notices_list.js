dd6e1b163c7784081d3cc7ed2b6162ea3a6aa55e
he2mo <sivakim91@gmail.com>
2021-12-02 17:03:15 +0900

    [server] MOD: 공지 검색기능  구현

--- server/controllers/notices/get_notices_list.js
+++ server/controllers/notices/get_notices_list.js
@@ -1,4 +1,6 @@
 const { users, notices } = require("../../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
 
 module.exports = async (req, res) => {
   // TODO 공지 리스트 요청 구현
@@ -6,6 +8,13 @@ module.exports = async (req, res) => {
   // page_num과 limit 기본값 설정
   const page_num = Number(req.query.page_num) || 1;
   const limit = Number(req.query.limit) || 10;
+
+  let where_obj = {};
+  if (req.query.type) where_obj.type = req.query.type;
+  if (req.query.notice_name)
+    where_obj.notice_name = {
+      [Op.like]: `%${req.query.notice_name}%`,
+    };
 
   let data;
   try {
@@ -20,6 +29,7 @@ module.exports = async (req, res) => {
         // "content",
         "created_at",
       ],
+      where: where_obj,
       include: {
         model: users,
         attributes: ["username"],
