90b216462c4fe05f1d0ab4b883ac3dda52a1657c
he2mo <sivakim91@gmail.com>
2021-12-02 16:36:57 +0900

    [server] MOD: 이메일 인증요청 코드 수정

--- server/controllers/auth/email_auth.js
+++ server/controllers/auth/email_auth.js
@@ -24,5 +24,5 @@ module.exports = async (req, res) => {
     return res.status(500).json({ data: err, message: "데이터베이스 에러" });
   }
 
-  return res.status(201).redirect(`${CLIENT_URL}`);
+  return res.status(201).json({ data: null, message: "ok" });
 };
