9fd2371ec42213c65550d5929f5fe066f0bdea6f
he2mo <sivakim91@gmail.com>
2021-12-03 19:58:09 +0900

    [server] MOD: 로그 기록 로직 테스트

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -14,29 +14,43 @@ const {
 } = require("./../models");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   // TODO
-  let data;
-  try {
-    bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
-      if (err) {
-        console.log(err);
-        return res
-          .status(500)
-          .json({ data: null, message: "데이터베이스 에러" });
-      }
+  if (!req.cookies.token) {
+    return res
+      .status(400)
+      .json({ data: null, message: "토큰이 존재하지 않습니다." });
+  }
 
-      data = await users.create({
-        ...req.body,
-        password: hash,
-      });
-    });
+  const token = req.cookies.token.access_token;
+  let decoded;
+  try {
+    decoded = jwt.verify(token, process.env.ACCESS_SECRET);
   } catch (err) {
     console.log(err);
+    return res
+      .status(401)
+      .json({ data: err, message: "토큰이 만료되었습니다." });
   }
-  // data = data.get({ plain: true });
-  console.log(data);
+  const { username } = decoded;
+  let data;
+  try {
+    let user_info = await users.findOne({
+      attributes: [["id", "user_id"]],
+      where: {
+        username: username,
+      },
+    });
+    user_info = user_info.get({ plain: true });
+    const user_id = user_info.user_id;
+
+    // user_id 로 log 검색
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ data: err, message: "데이터베이스 에러" });
+  }
 
   return res.status(200).json({ data, message: "ok" });
   return res.status(400).json({ data: null, message: "잘못된 요청입니다." });
