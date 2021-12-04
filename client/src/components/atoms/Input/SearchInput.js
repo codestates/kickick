9951161f5327b185a857d0743b0903873d385e04
gozld5153 <gozld5153@naver.com>
2021-12-05 00:35:51 +0900

    [client] MOD: 검색기능 bug fixed

--- client/src/components/atoms/Input/SearchInput.js
+++ client/src/components/atoms/Input/SearchInput.js
@@ -11,8 +11,12 @@ export default function SearchInput({ handleSearch, handleInput, word }) {
         placeholder="검색어를 입력하세요."
         value={word}
         onChange={handleInput}
-        onKeyUp={(e) => {
-          if (e.code === "Enter") handleSearch(e);
+        onKeyPress={(e) => {
+          if (
+            (e.code === "Enter" && word) ||
+            (e.code === "NumpadEnter" && word)
+          )
+            handleSearch(e);
         }}
       />
       <FaSearch style={{ cursor: "pointer" }} onClick={handleSearch} />
--- client/src/store/reducers/boardReducer.js
+++ client/src/store/reducers/boardReducer.js
@@ -3,7 +3,7 @@ import { GET_LIST } from "../actions/postadd/boardList";
 export const boardReducer = (state = {}, action) => {
   switch (action.type) {
     case GET_LIST:
-      action.payload.data.map((el, i) => {
+      action.payload.data.map((el) => {
         let index = el.created_at.indexOf("T");
         let date = el.created_at.slice(0, index);
         el.created_at = date.replace(/-/g, ".");
