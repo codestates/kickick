aea7e14d955802c2b34d9e130a28690a1399fc6d
he2mo <sivakim91@gmail.com>
2021-12-06 12:38:38 +0900

    [server] tag 배열로 추가하게끔 코드 수정

--- server/controllers/tags/post_tags_info.js
+++ server/controllers/tags/post_tags_info.js
@@ -3,11 +3,11 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   // TODO 게시글에 태그 달기 구현
-  if (!(req.body.post_id && req.body.content)) {
-    return res.status(400).json({
-      data: null,
-      message: "post_id, content 중 누락된 항목이 있습니다.",
-    });
+  // 태그 배열로 받기
+  if (!req.body.tags) {
+    return res
+      .status(400)
+      .json({ data: null, message: "태그 정보가 존재하지 않습니다." });
   }
 
   if (!req.cookies.token) {
@@ -28,9 +28,7 @@ module.exports = async (req, res) => {
   }
 
   const { username } = decoded;
-  const post_id = req.body.post_id;
-  const content = req.body.content;
-  let data;
+  let data = [];
 
   try {
     // 토큰의 username으로 user_id 구함
@@ -40,8 +38,8 @@ module.exports = async (req, res) => {
         username: username,
       },
     });
-
     const user_id = user_info.get({ plain: true }).id;
+    const post_id = req.body.post_id;
 
     // user_id 가 post_id로 구한 post의 user_id 와 같은지 확인
     let post_info = await posts.findOne({
@@ -57,45 +55,49 @@ module.exports = async (req, res) => {
       return res.status(401).json({ data: err, message: "권한이 없습니다." });
     }
 
-    // 작성한 태그가 이미 존재하는지부터 확인
-    let tag_info = await tags.findOrCreate({
-      where: {
-        content: content,
-      },
-      defaults: {
-        content: content,
-      },
-    });
+    // 태그 하나씩 접근
+    for (let tag of req.body.tags) {
+      // 작성한 태그가 이미 존재하는지부터 확인
+      let tag_info = await tags.findOrCreate({
+        where: {
+          content: tag,
+        },
+        defaults: {
+          content: tag,
+        },
+      });
 
-    // 생성될 때는 배열로 들어오므로 분기 합쳐줌
-    if (Array.isArray(tag_info)) {
-      tag_info = tag_info[0];
+      // 생성될 때는 배열로 들어오므로 분기 합쳐줌
+      if (Array.isArray(tag_info)) {
+        tag_info = tag_info[0];
+      }
+
+      // tag_id 와 post_id로 posts_tags에 추가
+      // 똑같은 태그를 또 추가하는 경우를 피하기 위해 findOrCreate 사용
+      tag_info = tag_info.get({ plain: true });
+      const tag_id = tag_info.id;
+
+      await posts_tags.findOrCreate({
+        where: {
+          post_id: post_id,
+          tag_id: tag_id,
+        },
+        defaults: {
+          post_id: post_id,
+          tag_id: tag_id,
+        },
+      });
+      // id 명시적으로
+      tag_info.tag_id = tag_info.id;
+      delete tag_info.id;
+
+      data.push(tag_info);
     }
-
-    // tag_id 와 post_id로 posts_tags에 추가
-    // 똑같은 태그를 또 추가하는 경우를 피하기 위해 findOrCreate 사용
-    tag_info = tag_info.get({ plain: true });
-    const tag_id = tag_info.id;
-
-    await posts_tags.findOrCreate({
-      where: {
-        post_id: post_id,
-        tag_id: tag_id,
-      },
-      defaults: {
-        post_id: post_id,
-        tag_id: tag_id,
-      },
-    });
-    data = tag_info;
-
-    // id 명시적으로
-    data.tag_id = data.id;
-    delete data.id;
   } catch (err) {
     console.log(err);
     return res.status(500).json({ data: err, message: "데이터베이스 에러" });
   }
+  console.log(data);
 
   return res.status(201).json({ data: data, message: "ok" });
 };
--- server/controllers/test.js
+++ server/controllers/test.js
@@ -18,54 +18,9 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (req, res) => {
   // TODO
-  if (!req.cookies.token) {
-    return res
-      .status(400)
-      .json({ data: null, message: "토큰이 존재하지 않습니다." });
-  }
 
-  const token = req.cookies.token.access_token;
-  let decoded;
-  try {
-    decoded = jwt.verify(token, process.env.ACCESS_SECRET);
-  } catch (err) {
-    console.log(err);
-    return res
-      .status(401)
-      .json({ data: err, message: "토큰이 만료되었습니다." });
-  }
-  const { username } = decoded;
-  let data;
-  try {
-    let user_info = await users.findOne({
-      attributes: [["id", "user_id"]],
-      where: {
-        username: username,
-      },
-    });
-    user_info = user_info.get({ plain: true });
-    const user_id = user_info.user_id;
+  console.log(req.body);
 
-    // user_id 로 log 검색
-    let log_info = await logs.findAll({
-      attributes: [
-        ["id", "log_id"],
-        "user_id",
-        "type",
-        "content",
-        "created_at",
-      ],
-      where: {
-        user_id: user_id,
-      },
-    });
-    log_info = log_info.map((el) => el.get({ plain: true }));
-    data = log_info;
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ data: err, message: "데이터베이스 에러" });
-  }
-
-  return res.status(200).json({ data: data, message: "ok" });
+  return res.status(200).json({ data: req.body, message: "ok" });
   return res.status(400).json({ data: null, message: "잘못된 요청입니다." });
 };
