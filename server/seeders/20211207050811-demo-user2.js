479275b24452d7b6b6a94aaa312e1ddc995ecf6a
he2mo <sivakim91@gmail.com>
2021-12-07 14:14:54 +0900

    [server] ADD: 관리자 계정 시드 추가 및 킥머니 증가

--- server/seeders/20211207050811-demo-user2.js
+++ server/seeders/20211207050811-demo-user2.js
@@ -5,7 +5,7 @@ module.exports = {
     return await queryInterface.bulkInsert("users", [
       {
         type: "admin",
-        username: "demouser",
+        username: "demouser2",
         email: "demouser@kickick.net",
         password:
           "$2b$10$B8IH2dfUY41gRNYwfgy4meX7zGjcpp7VXDfvxGOO0/snb7kAVxTX2",
