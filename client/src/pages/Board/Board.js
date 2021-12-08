bcc8df6a4ca93c4bb15ded3bd597f2f8e79a4e73
gozld5153 <gozld5153@naver.com>
2021-12-09 03:19:34 +0900

    [client] MOD: board page bug fixed

--- client/src/pages/Board/Board.js
+++ client/src/pages/Board/Board.js
@@ -6,8 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPostsList } from "../../apis/posts";
 import { getList } from "../../store/actions/postadd/boardList";
 import { getCategoryAction, resetTag } from "../../store/actions/postadd";
-
-import { TotalSearch, BoardBottom, BoardTop } from "../../components";
+import { selectPageAction } from "../../store/actions/pagination";
+import { TotalSearch, BoardTop, PostList } from "../../components";
 import BoardSkeleton from "./BoardSkeleton";
 
 export default function Board() {
@@ -18,9 +18,6 @@ export default function Board() {
   const stateOnoff = useSelector((state) => state.onoff);
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
-  const [selectPage, setSelectPage] = useState(
-    stateOnoff.goback ? (state.page ? state.page : 1) : 1
-  );
 
   useEffect(() => {
     dispatch(getCategoryAction(category));
@@ -47,6 +44,11 @@ export default function Board() {
       dispatch(resetTag());
       getPostsList({ category: apiCategory, limit: 20 })
         .then((data) => {
+          dispatch(
+            selectPageAction(
+              stateOnoff.goback ? (state.page ? state.page : 1) : 1
+            )
+          );
           dispatch(getList(data.data));
         })
         .then(() => setLoading(false))
@@ -61,7 +63,7 @@ export default function Board() {
       <Container>
         <BoardContainer>
           <TotalSearch setLoading={setLoading} />
-          <BoardBottom />
+          <PostList type="freepost" />
         </BoardContainer>
       </Container>
     </>
