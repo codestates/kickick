179135feb48b870cf80fdab55bf13bf0aa8fb93f
he2mo <sivakim91@gmail.com>
2021-12-08 13:16:56 +0900

    [server] MOD: 즐겨찾기 없어도 값이 오는 버그 수정

--- server/controllers/favorites/get_favorites_info.js
+++ server/controllers/favorites/get_favorites_info.js
@@ -39,6 +39,8 @@ module.exports = async (req, res) => {
       offset: limit * (page_num - 1),
       limit: limit,
       distinct: true,
+      order: [["id", "DESC"]],
+      where: {},
       include: [
         {
           model: posts,
@@ -49,6 +51,7 @@ module.exports = async (req, res) => {
             "post_name",
             "view_count",
           ],
+          required: true,
           include: [
             {
               model: users,
