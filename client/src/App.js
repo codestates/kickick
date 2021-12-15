f47653f88bc09834a446d7ffec0fd4186e431884
hanteagyu <snowone4426@gmail.com>
2021-12-15 14:51:55 +0900

    [client]MOD:socket.io 주소 환경변수 참조

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ import darkToLight from "./assets/images/darkToLight.png";
 
 export default function App() {
   const dispatch = useDispatch();
-  const socketClient = io("http://localhost:4000");
+  const socketClient = io(`${process.env.REACT_APP_API_URL}`);
   const isLogin = useSelector((state) => state.login.isLogin);
   const todayLogin = useSelector((state) => state.login.todayLogin);
   const themeMode = useSelector((state) => state.themeMode);
